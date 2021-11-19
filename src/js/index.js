d514ec5a213220f02a9880c42b04b26f79f2efd0
SeungHwan-Lee <sh981013s@gmail.com>
2021-11-19 23:23:21 +0900

    feat: create and update lists using localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,17 @@
   // dqs shortcut
   const $ = (selector) => document.querySelector(selector);
 
+  const menu = [];
+
+  const store = {
+    setLocalStorage(menu) {
+      localStorage.setItem('menu', JSON.stringify(menu));
+    },
+    getLocalStorage() {
+      localStorage.getItem('menu');
+    },
+  };
+
   const updateMenuCnt = () => {
     const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
     $('.menu-count').innerText = `총 ${menuCount} 개`;
@@ -15,9 +26,12 @@
     const espressoMenuName = document.querySelector(
       '#espresso-menu-name'
     ).value;
-    const menuItemTemplate = (espressoMenuName) => {
-      return `<li class="menu-list-item d-flex items-center py-2">
-                      <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+    menu.push({ name: espressoMenuName });
+    store.setLocalStorage(menu);
+    const template = menu
+      .map((item, idx) => {
+        return `<li data-menu-id="${idx}" class="menu-list-item d-flex items-center py-2">
+                      <span class="w-100 pl-2 menu-name">${item.name}</span>
                       <button
                         type="button"
                         class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -32,19 +46,24 @@
                       </button>
                     </li>
                     `;
-    };
-    $('#espresso-menu-list').insertAdjacentHTML(
-      'beforeend',
-      menuItemTemplate(espressoMenuName)
-    );
+      })
+      .join('');
+    $('#espresso-menu-list').innerHTML = template;
     updateMenuCnt();
     $('#espresso-menu-name').value = '';
   };
 
   const updateAndDeleteMenuName = (e) => {
+    const menuId = e.target.closest('li').dataset.menuId;
     if (e.target.classList.contains('menu-edit-button')) {
       const $menuName = e.target.closest('li').querySelector('.menu-name');
-      $menuName.innerText = prompt('메뉴명을 수정하세요', $menuName.innerText);
+      const updatedMenuName = prompt(
+        '메뉴명을 수정하세요',
+        $menuName.innerText
+      );
+      menu[menuId].name = updatedMenuName;
+      store.setLocalStorage(menu);
+      $menuName.innerText = updatedMenuName;
     }
     if (e.target.classList.contains('menu-remove-button')) {
       if (confirm('정말 삭제하겠습니까?')) {
